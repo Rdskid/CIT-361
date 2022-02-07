778276457d5868fa9412339a16b9bcd65c2d7b11
Rdskid <dskidmorelds@gmail.com>
2022-02-07 15:10:43 -0700

    commit

---
+++ week6/network.ps1
@@ -0,0 +1,10 @@
+<#
+Program Name : network
+Date: 02/07/2022
+Author: Ryan Skidmore
+Corse: CIT361
+I, Ryan Skidmore, affirm that I wrote this script as original work completed by me.
+#> 
+function Get-IPNetwork {
+
+}
